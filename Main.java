61d9dba95879521de40d6e3643bf504e27b8a787
spencer596 <spencer5980@gmail.com>
2024-01-20 13:16:40 -0700

    revision three (in master)

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 public class Main
 {
-    //this is the chronological revision 1 in master
+    //this is the chronological revision 3 in master
 
     public static void main(String[] args)
     {
         System.out.println("Hello world");
     }
 
+    public void methodInMaster()
+    {
+        System.out.println("Method in master");
+    }
 }
