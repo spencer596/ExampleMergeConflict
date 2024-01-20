8f4d58d492a92e79c765830bccf3c64920bc24ea
spencer596 <spencer5980@gmail.com>
2024-01-20 13:15:47 -0700

    revision two (in branch-feature)

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 public class Main
 {
-    //this is the chronological revision 1 in master
+    //this is the chronological revision 2 in branch-feature
 
     public static void main(String[] args)
     {
         System.out.println("Hello world");
     }
 
+    public void methodInBranch()
+    {
+        System.out.println("Method in branch");
+    }
 }
