dae18c498464fe2329c7fccfbfbfe56f70388a03
spencer596 <spencer5980@gmail.com>
2024-01-20 13:14:51 -0700

    revision one (in master)

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    //this is the chronological revision 1 in master
+
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world");
+    }
+
+}
